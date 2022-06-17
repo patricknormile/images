ba7c1e3b13ebf0ab44cd0e8edb3b1ddbf09d6cac
patricknormile <patnormil@gmail.com>
2022-06-16 21:01:21 -0600

    create processor object

--- image_processor.py
+++ image_processor.py
@@ -7,7 +7,12 @@ Created on Thu Jun 16 18:59:21 2022
 """
 import numpy as np
 from PIL import Image
+from pathlib import Path
 from skimage import io
+from sklearn.cluster import KMeans, SpectralClustering, Birch
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import os, sys
 
 class ImageProcessor() : 
     
@@ -17,7 +22,10 @@ class ImageProcessor() :
     
     def __init__(self, image_path) : 
         self.image_path = image_path
-        
+        self.image = io.imread(self.image_path)
+        self.arrfit = None
+        #self.altered_image_path = self.image_path.replace('raw_images',
+        #                                                  'altered_images')
     
     def resize_image(self, img, factor) : 
         """
@@ -26,3 +34,52 @@ class ImageProcessor() :
         pass
     
     def unscale_image(self, img, factor) :
+        pass
+    
+    def preprocess_color(self, image=None) : 
+        """
+        general preprocessor for color (resize to matrix)
+        """
+        if image is None :
+            image = self.image
+        self.original_shape = image.shape
+        self.arrfit = image.reshape((-1, 3))
+        return self.arrfit
+    
+    def show_image(self, image=None) : 
+        if image is None : 
+            image = self.image
+        io.imshow(image)
+        
+
+class ImageClusterer(ImageProcessor) : 
+    
+
+    
+    def cluster_image(self,alg,save_name=None,**kwargs) : 
+        if self.arrfit is None : 
+            self.preprocess_color(self.image)
+        
+        self.alg = alg(**kwargs)
+        self.alg.fit(self.arrfit)
+        labels = self.alg.labels_
+        centers = self.alg.cluster_centers_
+        self.color_clustered = centers[labels].reshape(self.original_shape)\
+            .astype('uint8')
+        
+        pthobj = Path(self.image_path)
+        if save_name is None :     
+            save_name = pthobj.stem + '.' + pthobj.suffix
+        
+        parent = pthobj.parent.parent
+        self.altered_image_path = os.path.join(parent,
+                                               'altered_images',
+                                               save_name)
+        io.imsave(self.altered_image_path,
+                  self.color_clustered)
+        
+        
+
+
+
+   
--- run.py
+++ run.py
@@ -9,12 +9,23 @@ Created on Thu Jun 16 18:47:51 2022
 import os, sys
 from pathlib import Path
 sys.path.append(cwd_path)
-import cluster_func
-cwd_path = os.path.abspath('.')
+import image_processor.ImageClusterer as IC
+from sklearn.cluster import KMeans, SpectralClustering, Birch
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
-raw_path = os.path.join(cwd_path , 'raw_images')
-raw_image_path = lambda x : os.path.join(raw_path, x)
 
-Path(os.path.join(cwd_path, 'altered_images')).mkdir(exist_ok=True)
+if __name__ == '__main__' : 
+    cwd_path = os.path.abspath('.')
+    
+    raw_path = os.path.join(cwd_path , 'raw_images')
+    raw_image_path = lambda x : os.path.join(raw_path, x)
+    
+    Path(os.path.join(cwd_path, 'altered_images')).mkdir(exist_ok=True)
+    
+    ex_file = 'drytool.jpg'
+    ex = IC(raw_image_path(ex_file))
+    ex.cluster_image(KMeans, n_clusters=4)
+    ex.show_image()
 
 
