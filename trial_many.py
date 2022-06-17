ed8cea256f6f1a028f35bdeb7531d6f80d31d3e3
patricknormile <patnormil@gmail.com>
2022-06-16 21:48:57 -0600

    loop through many iterations

--- trial_many.py
+++ trial_many.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 16 18:47:51 2022
+Created on Thu Jun 16 21:31:48 2022
 
 @author: patricknormile
 """
@@ -13,23 +13,22 @@ from sklearn.cluster import KMeans, SpectralClustering, Birch, BisectingKMeans,\
     MiniBatchKMeans
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from itertools import product
 
-
-if __name__ == '__main__' : 
+if __name__ == '__main__' :
     cwd_path = os.path.abspath('.')
     sys.path.append(cwd_path)
 
     raw_path = os.path.join(cwd_path , 'raw_images')
     raw_image_path = lambda x : os.path.join(raw_path, x)
-    
     Path(os.path.join(cwd_path, 'altered_images')).mkdir(exist_ok=True)
     
-    ex_file = 'summerrmnp.jpg'
-    ex = IC(raw_image_path(ex_file))
-    ex.cluster_image(MiniBatchKMeans,save_name='summer_mbkm_2.jpg', 
-                     n_clusters=2,
-                     random_state=9234,
-                     )
-    ex.show_image(ex.color_clustered)
-
-
+    all_imgs = [*os.walk(raw_path)][0][2]
+    all_k = [*range(2,25)]
+    for i,k in product(all_imgs, all_k) : 
+        fname = i[:i.find('.jpg')]
+        ic = IC(raw_image_path(i))
+        ic.cluster_image(MiniBatchKMeans,
+                         save_name=f"{fname}_mbkm_{k}",
+                         n_clusters=k,
+                         random_state=923829)
